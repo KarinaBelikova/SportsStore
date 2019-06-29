d69f5bf36c31ed9b2e76c8c57d635242c60e9825
Karina Belikova <kiit15.belikova@gmail.com>
2019-06-29 22:12:45 +0300

    navigation + tests

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -17,10 +17,11 @@ namespace SportsStore.Controllers
             _repository = repo;
         }
 
-        public ViewResult List(int productPage = 1)
+        public ViewResult List(string category, int productPage = 1)
             => View(new ProductsListViewModel
             {
                 Products = _repository.Products
+                .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductId)
                 .Skip((productPage - 1) * PageSize)
                 .Take(PageSize),
@@ -28,8 +29,10 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    TotalItems = _repository.Products.Count()
-                }
+                    TotalItems = category == null ? _repository.Products.Count() 
+                    : _repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
             });               
     }
 }
